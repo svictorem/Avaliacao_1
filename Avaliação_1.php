a72b49d9de893f70bfdf83649cce8edb2f0565ca
Victor Emanuel <129910747+svictorem@users.noreply.github.com>
2026-02-24 16:01:26 -0300

    Computeiros - AVC1

--- Avaliação_1.php
+++ Avaliação_1.php
@@ -3,18 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Computeiros - N1</title>
+    <title>Computaria - N1</title>
 </head>
+    <h3><em>Av - 1</em></h3>
 <body>
     <?php 
-        echo "Olá";
-    
-    
-    
-    
-    
-    
-    
+        class Carro {
+            //atributos do objeto
+            public $marca;
+            public $modelo;
+            public $ano;
+            public $cor;
+            public $velocidade =0;
+
+            // primeiro metodo
+            public function acelerar(){
+                $this -> velocidade +=10;
+                echo "O carro acelerou. A velociade e igual: " . $this->velocidade . "Km/h <br>";
+            }
+
+            //segundo metodo
+            public function frear(){
+                if($this -> velocidade >=10){
+                    $this->velocidade -=10;
+                }
+                echo "O carro desacelerou. Velociade e igual: " . $this->velocidade . "Km/h <br>";
+            }
+            
+        }
+        //Objeto
+        $meuCarro = new Carro();
+
+        //Metodos
+        $meuCarro->marca = "Toyota";
+        $meuCarro->acelerar();
+        $meuCarro->acelerar(); 
+        $meuCarro->acelerar();
+        $meuCarro->frear();
+        $meuCarro->frear();
     
     
     
